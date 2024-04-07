640e29f25ea0116c01298b800594200599c1ef94
kansas <jorgepad002@gmail.com>
2024-04-07 17:43:48 -0600

    cambie el redireccionamiento de las paginas, se uso el router que proporciona angular

--- proyectoCIIT/src/app/components/admin/admin.component.ts
+++ proyectoCIIT/src/app/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  redireccion(pagina:string){
+    switch (pagina) {
+      case "cliente":
+        this.router.navigate(['home/cliente'])
+        break;
+      case "cabana":
+        this.router.navigate(['home/cabana'])
+        break;
+      case "reservacion":
+        this.router.navigate(['home/reservacionAdmin'])
+        break;
+      case "cobros":
+        this.router.navigate(['home/cobros'])
+        break;
+      case "ofertas":
+        this.router.navigate(['home/ofertas'])
+        break;
+      default:
+        console.log("Ocurrio un error");
+        break;
+    }
+  }
 }
